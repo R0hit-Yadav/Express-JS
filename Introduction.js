aa6b3ff7f18fc45e650c5e3a0a6ae4d3b981eac5
R0hit-Yadav <rohitkyadav2312@gmail.com>
2024-06-13 22:06:44 +0530

    Express JS

---
+++ Introduction.js
@@ -0,0 +1,9 @@
+// Express is a fast,assertive,essential and moderate web framework of node.js
+
+// why use express 
+/*
+Ultra fast I/O
+Asynchronus and single threaded
+mvc like structure
+robust API makes routing esay
+*/
