bdffc50e0d1aa175edaeb27cb7e12071610e56e1
R0hit-Yadav <rohitkyadav2312@gmail.com>
2024-06-13 22:46:12 +0530

    Express JS

--- Introduction.js
+++ Introduction.js
@@ -14,3 +14,117 @@ app.get('/',function(req,res)
 {
     res.send("Hello WOrld!")
 });app.listen(5000)
+
+/* Response Method
+res.end()  -> End the response process
+res.jeson()  -> send a JSON response
+res.redirect()  -> Redirect a req
+res.render()  -> render a view template
+res.send() -> send a response of various types
+res.sendFile()  -> send a file as an octent stream
+*/
+
+const expr =require("express")
+const app = expr()
+app.get("/",(req,res)=>
+    {
+    res.set("content-type","text/plain")
+    res.send("<h1>Hello</h1>")
+})
+app.get("/about",(req,res)=>
+{
+    res.set("content-type","text/html")
+    res.write("Hello")
+    res.write("<h1> Hello from home</h1>")
+    res.send()
+});app.listen(4000)
+
+
+/*===>Routing
+Routing refres to determiing how an application responds to a 
+client request to particular endpoint, which is a URL(or path)
+and specific http request method (GET,POST, and so on ) */
+
+app.METHOD(path,handler)
+
+//respond with hello world on ht home page
+app.get('/',(req,res)=>
+{
+    res.send("Hello World!")
+})
+
+//respond to post requst on the root route , the application's home page:
+app.post('/',(req,res)=>
+{
+    res.send("Got a Post request")
+})
+
+// the app.all() function is used to route all types of https req
+
+
+//routes parameters
+var express = require("express")
+var app = express()
+app.get('/:id',function(req,res)
+{
+    res.send("The id you specified is "+req.params.id)
+});app.listen(5000)
+//https://localhost:5000/123 print 123 u can chnage in ural id
+
+//for multiple para
+var express= require("express")
+var app = express()
+app.get('/things/:name/:id',function(req,res)
+{
+    res.send('id: '+req.params.id + ' and name: '+req.params.name)
+    res.send(req.params)// from this all para access
+});app.listen(3000)
+
+// app.use() -> function is used to amount the specified middleware
+// function at the path which is being specified 
+// syntax: app.use(path,callback)
+
+//sending a json response
+app.get('/',(req,res)=>res.send("Hello World!"))
+res.json({username:"Dev"})
+
+//send JSON object using res.write
+const expr= require("express")
+const app = expr()
+student = {name:'xyz',age:31}
+app.get("/",(req,res)=>
+{
+    res.set("content-type","text/html")
+    res.write(JSON.stringify(student))
+    res.send()
+
+    res.send(student)//res.send auto converts data in strinf format
+
+});app.listen(5000)
+
+
+//EX- Write Express JS script to req server to display json object(array of object )
+// in table form on browser
+const expr=require("express")
+const app=expr()
+student={
+    A1:[{name:"xyz",id:1},
+        {name:"pqr",id:2},
+        {name:"mnc",id:3},
+        {name:"abc",id:4},
+    ]
+}
+app.get("/student",(req,res)=>
+{
+    res.set("content-type","text/html")
+    res.write("<center><table cellspacing='5px' cellpading='8px border='1px solid><tr><th>Name</th><th>ID</th></tr>'")
+    for(i of student.A1)
+        {
+            res.write("<tr><td>"+i.name+"</td>")
+            res.write("<td>"+JSON.stringify(i.id)+"</td></tr>")
+        }
+        res.write("<table></center>")
+        res.send()
+}).listen(5000)
+
+
