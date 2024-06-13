08d1a34e4a9b2781867449df3b65dcfed1d8f4b7
R0hit-Yadav <rohitkyadav2312@gmail.com>
2024-06-13 22:09:33 +0530

    Express JS

--- Introduction.js
+++ Introduction.js
@@ -7,3 +7,10 @@ Asynchronus and single threaded
 mvc like structure
 robust API makes routing esay
 */
+var express = require("express")
+var app=express()
+
+app.get('/',function(req,res)
+{
+    res.send("Hello WOrld!")
+});app.listen(5000)
